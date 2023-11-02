aab858c3c0fbc6c5ef9ef458c8874e76199459d4
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-11-02 19:53:46 -0400

    Hello, world

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sandbox for experimenting with MLOps processes',
+    author='Mavrick Laakso',
+    license='',
+)
